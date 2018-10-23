91f98eb46cd38f904d876b82a28603c49db2ec86
user <hosenaga@gmail.com>
2018-10-23 13:21:23 +0300

    TASK activity

--- app/src/main/java/com/example/user/deemataskmanger2018/MainTabsActivity.java
+++ app/src/main/java/com/example/user/deemataskmanger2018/MainTabsActivity.java
@@ -66,6 +66,8 @@ public class MainTabsActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent=new Intent( MainTabsActivity.this,AddTabsActivity.class);
+                startActivity(intent);
             }
         });
 
