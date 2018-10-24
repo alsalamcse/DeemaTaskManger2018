6479bb9fa5c20a15c13f31cb9e04b1dfc59792d6
user <hosenaga@gmail.com>
2018-10-24 08:56:18 +0300

    TASK activity

--- app/src/main/java/com/example/user/deemataskmanger2018/AddTabsActivity.java
+++ app/src/main/java/com/example/user/deemataskmanger2018/AddTabsActivity.java
@@ -2,12 +2,43 @@ package com.example.user.deemataskmanger2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 public class AddTabsActivity extends AppCompatActivity {
+    private EditText etTittle,etText,etDate;
+    private SeekBar skbrImportant,skbrNecessary;
+    private Button btnSave,btnDatePicker;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tabs2);
+        etTittle=findViewById(R.id.etTittle);
+        etText=findViewById(R.id.etTask);
+        etDate=findViewById(R.id.etDate);
+        skbrImportant=findViewById(R.id.skbrImportant);
+        skbrNecessary=findViewById(R.id.skbrNecessary);
+        btnDatePicker=findViewById(R.id.btnDataPicker);
+        btnSave=findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/example/user/deemataskmanger2018/MyTask.java
@@ -0,0 +1,73 @@
+package com.example.user.deemataskmanger2018;
+
+import java.util.Date;
+
+public class MyTask {
+    private String key;
+    private String tittle;
+    private String text;
+    private int important;
+    private int necessary;
+    private Date createdAt;
+    private Date dueDate;
+    public MyTask()
+    {
+
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getImportant() {
+        return important;
+    }
+
+    public void setImportant(int important) {
+        this.important = important;
+    }
+
+    public int getNecessary() {
+        return necessary;
+    }
+
+    public void setNecessary(int necessary) {
+        this.necessary = necessary;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+}
