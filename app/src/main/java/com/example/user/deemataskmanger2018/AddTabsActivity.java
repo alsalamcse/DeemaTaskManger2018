c88bf41bc0f2d97a288f27466841ffa1cd94c6eb
user <hosenaga@gmail.com>
2018-11-05 14:01:09 +0200

    datePicker and dataHandler

--- app/src/main/java/com/example/user/deemataskmanger2018/AddTabsActivity.java
+++ app/src/main/java/com/example/user/deemataskmanger2018/AddTabsActivity.java
@@ -1,17 +1,21 @@
 package com.example.user.deemataskmanger2018;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.SeekBar;
+
+import java.util.Calendar;
 
 public class AddTabsActivity extends AppCompatActivity {
     private EditText etTittle,etText,etDate;
     private SeekBar skbrImportant,skbrNecessary;
     private Button btnSave,btnDatePicker;
-
+    private int mYear,mMonth,mDay;
 
 
 
@@ -29,15 +33,48 @@ public class AddTabsActivity extends AppCompatActivity {
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                dataHandler();
 
             }
         });
-        btnDatePicker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
+    }
+
+    private void dataHandler() {
+        String Tittle=etTittle.getText().toString();
+        String Task=etText.getText().toString();
+        String  Date=etDate.getText().toString();
+       int important=skbrImportant.getProgress();
+       int Necessary=skbrNecessary.getProgress();
+       if(Tittle) {
+
+       }
+    }
+
+    public void onClick(View v) {
+
+        if (v == btnDatePicker) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            etDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
     }
 
 
